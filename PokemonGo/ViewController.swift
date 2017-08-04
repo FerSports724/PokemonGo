3f3ac38b703ebb749ba55d8d2241a5b7f5c38f0a
Fernando Vazquez Bernal <fernando.vazquezbernal@gmail.com>
2017-08-04 17:31:13 -0500

    Commit inicial de Pokemon Go.

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Fernando Vazquez Bernal on 04/08/17.
+//  Copyright © 2017 Fernando Vazquez Bernal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
